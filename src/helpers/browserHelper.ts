31072c122be59c9848379078c3f3d65cb7a61c9b
Viktar Bystrou <viktar.bystrou@refinitiv.com>
2023-06-29 12:30:23 +0300

    debug ci7

--- src/helpers/browserHelper.ts
+++ src/helpers/browserHelper.ts
@@ -1,20 +1,20 @@
-import playwright, {Browser, BrowserContext, Page} from 'playwright';
-
-export class BrowserHelper {
-    static browser: Browser;
-
-    static page: Page;
-
-    public static async init(): Promise<Browser> {
-        if (!BrowserHelper.browser) {
-            BrowserHelper.browser = await playwright.chromium.launch({
-                headless: false,
-                args: ['--start-maximized '],
-                slowMo: 1000,
-            });
-        }
-        return BrowserHelper.browser;
-    }
+// import playwright, {Browser, BrowserContext, Page} from 'playwright';
+//
+// export class BrowserHelper {
+//     static browser: Browser;
+//
+//     static page: Page;
+//
+//     public static async init(): Promise<Browser> {
+//         if (!BrowserHelper.browser) {
+//             BrowserHelper.browser = await playwright.chromium.launch({
+//                 headless: false,
+//                 args: ['--start-maximized '],
+//                 slowMo: 1000,
+//             });
+//         }
+//         return BrowserHelper.browser;
+//     }
 
     public static async openPage() {
         this.page = await (await BrowserHelper.browser.newContext({viewport: null})).newPage();
